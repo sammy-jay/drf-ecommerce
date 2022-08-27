08fb49fab2e8bd541d8c6853d7de6e6ad1bfbfd4
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-27 23:03:04 +0000

    customized the admin for the User table

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'first_name', 'last_name','is_active')
+    list_filter = ('is_active', 'username')
+    fields = [('email', 'password'), ('username', 'phone_number'), ('first_name', 'last_name')]
+
+admin.site.register(User, UserAdmin)
