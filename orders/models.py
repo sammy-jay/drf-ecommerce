fc6ebaec8cfbe91368610bea4cd650026137c0f6
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-27 23:17:54 +0000

    created order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class OrderModel(models.Model):
+
+    SIZES = (
+        ('SMALL', 'small'),
+        ('MEDIUM', 'medium'),
+        ('LARGE', 'large'),
+        ('EXTRA_LARGE', 'extra_large'),
+    )
+
+    ORDER_STATUS = (
+        ('PENDING', 'pending'),
+        ('IN_TRANSIT', 'inTransit'),
+        ('DELIVERED', 'delivered')
+    )
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    size = models.CharField(max_length=20, choices=SIZES, default=SIZES[0][0])
+    order_status = models.CharField(
+        max_length=20, choices=ORDER_STATUS, default=SIZES[0][0])
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.size + ' order by ' + self.customer.id
+    
